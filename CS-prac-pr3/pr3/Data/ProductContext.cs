a3379938bcc88e0b76ec0aaf8eb1f035f7a450b7
PetruhinYura <st8542342@stud.duikt.edu.ua>
2024-12-10 20:37:55 +0200

    Add files via upload

---
+++ CS-prac-pr3/pr3/Data/ProductContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pr3.Models;
+
+namespace pr3.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
